7161da179c62b5316f2bf5072bef1fe8af80549d
Ruben <ruben.pogosyan95@gmail.com>
2022-11-15 22:53:27 +0400

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Select Three Digit Number = ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number >= 100 && Number < 1000)
+{
+    Console.WriteLine((Number % 100) / 10);
+}
+else
+    Console.WriteLine("Wrong Number");
